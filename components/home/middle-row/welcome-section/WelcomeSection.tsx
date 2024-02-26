6687f350169abbab7017859d3ff4c7c1f2b44001
pfzoz <pedrozoz.sizaan@gmail.com>
2024-02-26 15:54:17 -0300

    contents

--- components/home/middle-row/welcome-section/WelcomeSection.tsx
+++ components/home/middle-row/welcome-section/WelcomeSection.tsx
@@ -4,7 +4,8 @@ export default function WelcomeSection() {
   return (
     <section className={styles.default}>
       <h2>Welcome to my blog!</h2>
-      <p className={styles.paragraphText}>{contents["welcome-section"]["description"]}</p>
+      <p className={styles.paragraphText}>{contents.welcomeSection.description}</p>
+      <p className={styles.paragraphText}>Sorry but this site is under construction at the moment :/</p>
     </section>
   );
 }
--- components/home/middle-row/welcome-section/contents.ts
+++ components/home/middle-row/welcome-section/contents.ts
@@ -1,8 +1,11 @@
 const contents = {
-    "welcome-section": {
-        "title": "Welcome to my blog!",
-        "description": "Hello, my name is Pedro Henrique Ferreira Zoz, and this is my personal blog. In this website you can check out my projects, updates and more."
-    }
+  welcomeSection: {
+    title: "Welcome to my blog!",
+    description:
+      "Hello, my name is Pedro Henrique Ferreira Zoz, and this is my personal blog. \
+      I'm a computer science student at Universidade Estadual do Oeste do Paraná, Paraná, Brazil. \
+      In this website you can check out my projects and updates.",
+  },
 };
 
 export default contents;
